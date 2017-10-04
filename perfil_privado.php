d05707395d5e14232b991f0e2c6d65956b8ec815
Panax97 <tcpanameno@gmail.com>
2017-10-03 22:39:41 -0600

    Base del Proyecto
    Los form hechos hasta ahora

---
+++ perfil_privado.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Post - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-post.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">DevCom Sv</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+        <div class="card my-4">
+            <h5 class="card-header">Notificaciones</h5>
+            <div class="card-body">
+              <!-- /.notificacionesp -->
+              <div class="panel-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item">
+                                    Mensajes sin leer
+                                    <span class="pull-right text-muted small"><em>4</em>
+                                    </span>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                     Nuevos Comentarios
+                                    <span class="pull-right text-muted small"><em>4</em>
+                                    </span>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    Nuevos Seguidores
+                                    <span class="pull-right text-muted small"><em>4</em>
+                                    </span>
+                                </a>
+                              </br>
+
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">Nuevo Post</button>
+                            </div>
+                            <!-- /.terminan las notificaciones -->
+                            <!-- /.list-group -->
+                        </div>
+
+                        <!-- /.Publicidad  --> 
+
+
+                        <div class="card my-4">
+            <h5 class="card-header">Publicidad</h5>
+            <div class="card-body">
+              <div class="input-group">
+                
+              </div>
+            </div>
+          </div>
+            </div>
+          </div>
+
+          <!-- /.termina publicidad-->
+
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+
+          <!-- Title -->
+          <h1 class="mt-4">BigPana</h1>
+          <center><img class="img-fluid rounded" src="img/ic.png" alt=""></center>
+
+          <!-- Author -->
+          <p class="lead">
+            <center><a>Nivel de Usuario</a></center>
+          </p>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <!-- /.tabla de puntos -->
+          <div class="container">            
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Puntos</th>
+        <th>Post</th>
+        <th>Seguidores</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>500</td>
+        <td>24</td>
+        <td>10</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<!-- /.Termina la tabla de puntos-->
+
+<!-- /.tabla de post-->
+
+          <hr>
+         <center> <h2 class="mt-4">Tabla de Publicaciones</h2></center>
+         <hr>
+
+          <!-- Preview Image -->
+           <div class="container">            
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Puntos</th>
+        <th>Comentarios</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Como Crear tu propio blog desde cero sin pedos alguno</td>
+        <td>24</td>
+        <td>10</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+          <hr>
+          <!-- /.termina tabla de post-->
+
+          <!-- Post Content -->
+          
+
+          <!-- Comments Form -->
+
+          <!-- Single Comment -->
+          
+
+          <!-- Comment with nested comments -->
+
+        <!-- Sidebar Widgets Column -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
