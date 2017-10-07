7333d3b2c670b539d2dab026e2ad695145756e44
Panax97 <tcpanameno@gmail.com>
2017-10-07 00:37:58 -0600

    Plantilla
    Forms Creados

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Post - Start Bootstrap Template</title>
+    <title>DevCom Sv</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -23,25 +23,23 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">DevCom Sv</a>
+        <!--<a href="#" class="navbar-brand"><img src="img/toro.jpg" style="margin-top:-25px;"></a> -->
+        <a class="navbar-brand" href="index.php">DevCom SV</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home
+              <a class="nav-link" href="index.php">Home
                 <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="#">Contactanos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+              <a class="nav-link" href="#">Perfil</a>
             </li>
           </ul>
         </div>
@@ -49,100 +47,215 @@
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
+
 
       <div class="row">
-        <div class="card my-4">
-            <h5 class="card-header">Opciones de Usuario</h5>
+        <div class="col-md-4">
+          <div class="card my-4">
+            <h5 class="card-header">Buscador</h5>
             <div class="card-body">
               <div class="input-group">
-                <button type="submit" class="btn btn-primary btn-lg btn-block">Enviar Mensaje</button>
-              </div>
-            </br>
-              <div class="input-group">
-               <button type="submit" class="btn btn-primary btn-lg btn-block">Seguir</button>
-              </div>
-              <div class="card my-4">
-            <h5 class="card-header">Publicidad</h5>
-            <div class="card-body">
-              <div class="input-group">
-                
+                <input type="text" class="form-control" placeholder="Search for...">
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Buscar</button>
+                </span>
+
               </div>
             </div>
           </div>
+          <div class="card my-6">
+            <h5 class="card-header"><center>Top 10</center></h5>
+            <div class="card-body">
+              <div class="container">
+
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Post</th>
+        <th>Puntos</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>El titulo no tiene que tener mas de 70 caracteres</td>
+        <td>99,999</td>
+      </tr>
+      <tr>
+        <td>Como crear tu primera pagina web Parte 1</td>
+        <td>55,529</td>
+      </tr>
+      <tr>
+        <td>Hacer tu negocio en facebook desde cero sin dinero ni nada</td>
+        <td>50,000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
             </div>
           </div>
+
+          <div class="card my-4">
+            <h5 class="card-header"><center>Publicidad</center></h5>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+
+        </div>
 
         <!-- Post Content Column -->
-        <div class="col-lg-8">
+        <div class="col-lg-5">
+           <h1 class="my-4">Titulo Breve
+            <small>Lenguaje</small>
+          </h1>
 
-          <!-- Title -->
-          <h1 class="mt-4">BigPana</h1>
-          <center><img class="img-fluid rounded" src="img/ic.png" alt=""></center>
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Hacer tu negocio en facebook desde cero sin dinero ni nada</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
 
-          <!-- Author -->
-          <p class="lead">
-            <center><a>Nivel de Usuario</a></center>
-          </p>
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
 
-          <hr>
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
 
-          <!-- Date/Time -->
-          <div class="container">            
-  <table class="table table-bordered">
+          <!-- Pagination -->
+          <ul class="pagination justify-content-center mb-4">
+            <li class="page-item">
+              <a class="page-link" href="#">&larr; Older</a>
+            </li>
+            <li class="page-item disabled">
+              <a class="page-link" href="#">Newer &rarr;</a>
+            </li>
+          </ul>
+
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-3">
+
+          <!-- Categories Widget -->
+          <div class="card my-5">
+            <h5 class="card-header">Categorias</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">Web Design</a>
+                    </li>
+                    <li>
+                      <a href="#">HTML</a>
+                    </li>
+                    <li>
+                      <a href="#">Java</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">JavaScript</a>
+                    </li>
+                    <li>
+                      <a href="#">CSS</a>
+                    </li>
+                    <li>
+                      <a href="#">SQL</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-5">
+            <h5 class="card-header"><center>Publicidad</center></h5>
+            <div class="card-body">
+              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            </div>
+          </div>
+          <div class="card my-6">
+            <h5 class="card-header"><center>Top 10</center></h5>
+            <div class="card-body">
+              <div class="container">
+
+  <table class="table table-condensed">
     <thead>
       <tr>
+        <th>Usuario</th>
         <th>Puntos</th>
-        <th>Post</th>
-        <th>Seguidores</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>500</td>
-        <td>24</td>
-        <td>10</td>
+        <td>ManuVill96</td>
+        <td>99,999</td>
+      </tr>
+      <tr>
+        <td>Kevin_Biker28</td>
+        <td>55,529</td>
+      </tr>
+      <tr>
+        <td>Pana_G97</td>
+        <td>50,000</td>
       </tr>
     </tbody>
   </table>
 </div>
 
-          <hr>
-         <center> <h2 class="mt-4">Tabla de Publicaciones</h2></center>
-         <hr>
+            </div>
+          </div>
 
-          <!-- Preview Image -->
-           <div class="container">            
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>Nombre</th>
-        <th>Puntos</th>
-        <th>Comentarios</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Como Crear tu propio blog desde cero sin pedos alguno</td>
-        <td>24</td>
-        <td>10</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-          <hr>
+        </div>
 
       </div>
       <!-- /.row -->
 
-    </div>
+
     <!-- /.container -->
 
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+        <p class="m-0 text-center text-white">Creado Por chiltex</p>
       </div>
       <!-- /.container -->
     </footer>
